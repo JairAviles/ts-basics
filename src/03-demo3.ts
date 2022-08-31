b5acb399caaabae4a78c7e6c891331f598293cc0
Jair Aviles <jair.aviles@segment.com>
2022-08-30 20:11:08 -0700

    add demo files

---
+++ src/03-demo3.ts
@@ -0,0 +1,11 @@
+(()=> {
+  let productName = 'Product One';
+  let productPrice = 123;
+
+  productName = 'new name';
+  productName.toUpperCase();
+
+  const myProductStock = 1000;
+})();
+
+
---
+++ src/03-typing.ts
@@ -0,0 +1,7 @@
+let productName = 'Product One';
+let productPrice = 123;
+
+productName = 'new name';
+productName.toUpperCase();
+
+const myProductStock = 1000;
