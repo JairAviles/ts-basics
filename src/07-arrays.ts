37516fb15ad9795f299b26d041343afed1aaa349
Jair Aviles <jair.aviles@segment.com>
2022-09-02 16:40:16 -0700

    add 07-arrays file

---
+++ src/07-arrays.ts
@@ -0,0 +1,22 @@
+(() => {
+  const prices = [1, 2, 3, 4, 5, 'hello', true]
+  // prices.push('xyz')
+  // prices.push(true)
+  // prices.push({})
+  // prices.push('6')
+  prices.push(6)
+  console.log('prices', prices)
+
+  const products = ['random', false]
+  // products.push(123)
+  console.log('products', products)
+
+  const mixed: (number | string | boolean | Object)[] = ['init', false]
+  mixed.push(2016)
+  mixed.push({}) // tmp
+  mixed.push([]) // tmp
+
+  const numbers = [1, 2, 3 , 6, 5, 4]
+  numbers.map(item => item * 2)
+
+})()
